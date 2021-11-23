c07965aa474186bc9173c1ff5b358cf1f26c3910
monicakj <79156195+monicakj@users.noreply.github.com>
2021-11-23 02:13:38 +0100

    update

---
+++ js/blog.js
@@ -0,0 +1,35 @@
+const url = 'https://bucketlisttravels.mskj.one/wp-json/wp/v2/posts';
+const blogContainer = document.querySelector (".blog-posts");
+
+async function getPosts() {
+    try {
+        const response = await fetch(url);
+        const getResults = await response.json();
+        createHTML(getResults);
+    }
+    catch(error) {
+        console.log("An error has ocurred.");
+        blogContainer.innerHTML = displayError();
+    }
+}
+
+getPosts();
+
+function createHTML(posts){
+    posts.forEach(function(posts) {
+        console.log(posts);
+
+        blogContainer.innerHTML += 
+        `
+        <div class="posts">
+         <a href="/specificpage.html?id=${posts.id}">
+        <h1>${posts.title.rendered}</h1>
+        </a>
+
+        <div class="posts-content">
+        ${posts.content.rendered}
+        </div>
+        </div>
+        `;
+    })
+}
